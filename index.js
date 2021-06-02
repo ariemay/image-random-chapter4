698c7f054817740f584aa31eb9c3942553b6198c
samx23 <skalexsong@gmail.com>
2021-06-02 20:53:01 +0700

    Update : tambah kondisi ketika hasil sama + prettier code

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"]
-const imagePath = "assets/images/"
+const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"];
+const imagePath = "assets/images/";
 
-let image1 = document.getElementById("image1")
-let image2 = document.getElementById("image2")
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
+const changeButton = document.getElementById("changeButton");
 
 function getRandomImage() {
-    let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)]
-    let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)]
-    image1.src = imagePath + randomImage1
-    image2.src = imagePath + randomImage2
+  let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)];
+  let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)];
+  image1.src = imagePath + randomImage1;
+  image2.src = imagePath + randomImage2;
+
+  if (image1.src === image2.src) {
+    console.log("Tee Tooot");
+    changeButton.style = "background-color: red;";
+  } else {
+    changeButton.style = "background-color: unset;";
+  }
 }
 
 // TODO:
@@ -18,4 +26,4 @@ maka tombol CHANGE akan berubah menjadi warna merah
 2. Hasil dari code challenge dipush ke github di branch masing2.
 */
 
-const changeButton = document.getElementById("changeButton").onclick = getRandomImage
+changeButton.onclick = getRandomImage;
