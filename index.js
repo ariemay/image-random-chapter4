7905e0449e29739fd111dca5e6b405906edc2b67
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-02 21:44:25 +0700

    modified file

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"]
-const imagePath = "assets/images/"
+const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"];
+const imagePath = "assets/images/";
 
-let image1 = document.getElementById("image1")
-let image2 = document.getElementById("image2")
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
 
 function getRandomImage() {
-    let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)]
-    let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)]
-    image1.src = imagePath + randomImage1
-    image2.src = imagePath + randomImage2
+  let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)];
+  let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)];
+  image1.src = imagePath + randomImage1;
+  image2.src = imagePath + randomImage2;
+  if (image1.src === image2.src) {
+    document.querySelector("button").style.backgroundColor = "red";
+  } else {
+    document.querySelector("button").style.backgroundColor = "white";
+  }
 }
+
+// document.querySelector("#changeButton").addEventListenerd("click", function () {
+//   document.querySelector("#changeButton").style.backgroundColor = "#60b347";
+// });
 
 // TODO:
 /*
@@ -18,4 +27,4 @@ maka tombol CHANGE akan berubah menjadi warna merah
 2. Hasil dari code challenge dipush ke github di branch masing2.
 */
 
-const changeButton = document.getElementById("changeButton").onclick = getRandomImage
+const changeButton = (document.getElementById("changeButton").onclick = getRandomImage);
