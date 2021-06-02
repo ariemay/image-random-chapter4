75e36d4fc528dfce40354a18f6760224836f8adc
tiko <tiko.pb@gmail.com>
2021-06-02 21:01:18 +0700

    fixing bug

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const imagePath = "assets/images/"
 let image1 = document.getElementById("image1")
 let image2 = document.getElementById("image2")
 
+const changeButton = document.getElementById("changeButton").onclick = getRandomImage
+
 function getRandomImage() {
+    document.getElementById("changeButton").style.background='#FFFFFF';
     let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)]
     let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)]
     image1.src = imagePath + randomImage1
     image2.src = imagePath + randomImage2
+    if(randomImage1 == randomImage2){
+        console.log("same image")
+        document.getElementById("changeButton").style.background='#FF0000';
+    }
 }
 
 // TODO:
