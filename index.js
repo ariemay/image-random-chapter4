d40cba712e8654b0ad50f4111db968afc64c2ea6
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-06-02 21:25:01 +0700

    change color button

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"]
-const imagePath = "assets/images/"
+const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"];
+const imagePath = "assets/images/";
 
-let image1 = document.getElementById("image1")
-let image2 = document.getElementById("image2")
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
 
 function getRandomImage() {
-    let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)]
-    let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)]
-    image1.src = imagePath + randomImage1
-    image2.src = imagePath + randomImage2
+  const changeButtonColor = document.querySelector("#changeButton button");
+  let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)];
+  let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)];
+  image1.src = imagePath + randomImage1;
+  image2.src = imagePath + randomImage2;
+  if (image1.src == image2.src) {
+    changeButtonColor.style.color = "red";
+  } else {
+    changeButtonColor.style.color = "black";
+  }
+  //   console.log(image1);
 }
 
 // TODO:
@@ -18,4 +25,4 @@ maka tombol CHANGE akan berubah menjadi warna merah
 2. Hasil dari code challenge dipush ke github di branch masing2.
 */
 
-const changeButton = document.getElementById("changeButton").onclick = getRandomImage
+const changeButton = (document.getElementById("changeButton").onclick = getRandomImage);
