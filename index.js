6ff61bfd727532a3efd8570cc580229991332d54
Arie May Wibowo <ariemayw@gmail.com>
2021-06-02 20:35:45 +0700

    init code challenge

---
+++ index.js
@@ -0,0 +1,14 @@
+const Animals = ["horse.png", "lion.jpeg", "panda.jpeg", "pinguin.jpeg"]
+const imagePath = "assets/images/"
+
+let image1 = document.getElementById("image1")
+let image2 = document.getElementById("image2")
+
+function getRandomImage() {
+    let randomImage1 = Animals[Math.floor(Math.random() * Animals.length)]
+    let randomImage2 = Animals[Math.floor(Math.random() * Animals.length)]
+    image1.src = imagePath + randomImage1
+    image2.src = imagePath + randomImage2
+}
+
+const changeButton = document.getElementById("changeButton").onclick = getRandomImage
